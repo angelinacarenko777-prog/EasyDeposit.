95def78752e6c83ead8f09ebd4ceff0e0baaba55
Angelina Tsarenko <angelinacarenko777@gmail.com>
2026-03-19 14:37:56 +0200

    Added initial deposit data

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const deposits = [
+  {
+    bank: "PrivatBank",
+    percent: 15,
+    term: 12,
+    currency: "UAH"
+  },
+  {
+    bank: "Monobank",
+    percent: 16,
+    term: 6,
+    currency: "UAH"
+  },
+  {
+    bank: "Oschadbank",
+    percent: 14,
+    term: 24,
+    currency: "USD"
+  }
+];
