4e68f759fa512a31bdcf1820c1587f5233fbb394
Angelina Tsarenko <angelinacarenko777@gmail.com>
2026-03-19 14:39:05 +0200

    Implement deposit cards rendering in deposits.js
    Added functionality to display deposits in the UI.

--- js/deposits.js
+++ js/deposits.js
@@ -1,1 +1,16 @@
 console.log("Deposits script loaded");
+const container = document.getElementById("deposit-list");
+
+deposits.forEach(deposit => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <h3>${deposit.bank}</h3>
+    <p>Відсоток: ${deposit.percent}%</p>
+    <p>Термін: ${deposit.term} місяців</p>
+    <p>Валюта: ${deposit.currency}</p>
+  `;
+
+  container.appendChild(card);
+});
